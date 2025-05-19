dfdb176355abad8ee0a35758fb02229eeb54caab
SigmaBoyDiHoc <nguoigiulopb@gmail.com>
2025-05-19 15:37:37 +0700

    Add files via upload

---
+++ ctrl+click.lua
@@ -0,0 +1,26 @@
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+local UserInputService = game:GetService("UserInputService")
+
+local ctrlDown = false
+
+UserInputService.InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.LeftControl or input.KeyCode == Enum.KeyCode.RightControl then
+        ctrlDown = true
+    end
+end)
+
+UserInputService.InputEnded:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.LeftControl or input.KeyCode == Enum.KeyCode.RightControl then
+        ctrlDown = false
+    end
+end)
+
+mouse.Button1Down:Connect(function()
+    if ctrlDown and mouse.Target then
+        local character = player.Character
+        if character and character:FindFirstChild("HumanoidRootPart") then
+            character.HumanoidRootPart.CFrame = CFrame.new(mouse.Hit.p + Vector3.new(0, 3, 0))
+        end
+    end
+end)
